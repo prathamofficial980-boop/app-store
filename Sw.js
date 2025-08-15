b1e0ce61e49a3e340cd912ca3553b631ba3e50d0
prathamofficial980-boop <prathamofficial980@gmail.com>
2025-08-15 14:36:58 +0530

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,28 @@
+// Simple service worker for offline caching
+const CACHE = 'apphub-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e)=>{
+  const url = new URL(e.request.url);
+  if(url.origin === location.origin){
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }else{
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./')));
+  }
+});
